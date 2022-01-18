20f94287cd8285aeebc403f2fc65f4f084691ad9
Rafael Dalarosa <rafaelhcd@hotmail.com>
2022-01-18 14:52:15 -0300

    Add Template

--- src/controllers/codeeducation.js
+++ src/controllers/codeeducation.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const methods = require('../methods/codeeducation');
+const ejs = require('ejs');
 
 router.get('/', getAll);
-router.post('/', insertRow);
 
 function getAll(req, res, next) {
     methods.getAll()
     .then(([rows]) => {
-        res.json(rows);
-    })
-}
+        let listofnames = [];
+        for (let i = 0; i< rows.length; i++){
+            listofnames.push(rows[0][0].name)
+        }
 
-function insertRow(req, res) {
-    const nome = req.body.nome;
-    //Nome das variaveis que estão sendo passadas para o método deve ser o mesmo do nome da coluna do Banco de Dados
-    methods.insertRow({nome}).then(([rows]) => {
-        res.send({message: 'adicionado com sucesso'});
+        const html = ejs.renderFile(__dirname + "template.ejs",
+        {name: listofnames}); 
+
+        res.send(html);
     })
 }
 
--- src/methods/codeeducation.js
+++ src/methods/codeeducation.js
@@ -4,8 +4,4 @@ const getAll = _ => {
     return conn.promise().query('SELECT * FROM people');
 }
 
-const insertRow = data => {
-    return conn.promise().query(`INSERT INTO people SET ?`, data);
-}
-
-module.exports = {getAll, insertRow};
+module.exports = {getAll};
