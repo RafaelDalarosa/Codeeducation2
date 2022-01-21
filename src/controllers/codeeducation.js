615b7370098caea3b653e4af5cb932c1eebd558a
Rafael Dalarosa <rafaelhcd@hotmail.com>
2022-01-21 11:06:08 -0300

    Update Node Project

--- src/controllers/codeeducation.js
+++ src/controllers/codeeducation.js
@@ -9,13 +9,15 @@ async function getAll(req, res, next) {
 
     let listofnames = await methods.getAll();
     listofnames = listofnames[0];
+
+    let names = [];
         
     for (let i = 0; i< listofnames.length; i++){
-        listofnames.push(listofnames[i].name);
+        names.push(listofnames[i].name);
     }
 
     const html = await ejs.renderFile(__dirname + "/template.ejs",
-    {name: listofnames}); 
+    {name: names}); 
 
     res.status(200).send(html);
     
